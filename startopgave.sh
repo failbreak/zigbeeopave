42b3e360e15b75cd909abde805368793b579b098
failbreak <69219123+failbreak@users.noreply.github.com>
2024-12-18 10:24:38 +0100

    Create startopgave.sh

---
+++ startopgave.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# Im lazy so im making a script to run things for me
+if [ -d ./zigbeeopgave ]; then
+  cd ./zigbeeopgave
+  echo "Directory exists."
+  getfilesfromgit
+  echo "Finished downloading files"
+else
+  mkdir zigbeeopgave
+  
+fi
+
+getfilesfromgit(){
+  echo "Downloading config files"
+if curl https://raw.githubusercontent.com/failbreak/zigbeeopave/refs/heads/main/docker-compose.yml -o docker-compose.yml; then
+  echo "Finished Downloading docker compose"
+fi
+if curl https://raw.githubusercontent.com/failbreak/zigbeeopave/refs/heads/main/configuration.yml -o configuration.yml; then
+  echo "Finished Downloading configuration"
+fi
+}
+
+runzigbee(){
+docker compose up -d
+}
